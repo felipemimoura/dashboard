2770c02948789175da0decd715bedd7f5a58a912
Felipe Moura <felipemimoura@gmail.com>
2022-05-11 16:09:49 -0300

    feat: config to axios

---
+++ src/services/auth.js
@@ -0,0 +1,22 @@
+export default httpClient => ({
+  login: async ({ email, password }) => {
+    // Fazendo login na aplicação
+    const response = await httpClient.post('/auth/login', {
+      email, password
+    })
+
+    // Lidando com erros
+    let erros = null
+    if (!response.data) {
+      erros = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      }
+    }
+
+    return {
+      data: response.data,
+      erros
+    }
+  }
+})
