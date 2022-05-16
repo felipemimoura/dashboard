58a39c2af26f9c4963a89dd5d6defc2c0696e24d
Felipe Moura <felipemimoura@gmail.com>
2022-05-16 11:43:25 -0300

    feat: add user verification at all aplication

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <modal-factory />
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
+import { watch } from 'vue'
 import ModalFactory from './components/ModalFactory'
+import { useRoute, useRouter } from 'vue-router'
+import services from './services'
 
 export default {
-  components: { ModalFactory }
-}
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
 
+    // Verificando sempre que a rota mudar
+    watch(
+      () => route.path,
+      async () => {
+        if (route.meta.hasAuth) {
+          const token = window.localStorage.getItem('token')
+
+          if (!token) {
+            router.push({ name: 'Home' })
+            return
+          }
+
+          const { data } = await services.user.getMe()
+          console.log(data)
+        }
+      }
+    )
+  }
+}
 </script>
