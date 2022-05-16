d2aa54bdc1f2bd990d437b59087c9c94897f4bc5
Felipe Moura <felipemimoura@gmail.com>
2022-05-16 14:24:19 -0300

    feat: add loading function from global context

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@ import router from '@/router'
 import axios from 'axios'
 import AuthService from './auth'
 import UsersService from './users'
+import { setLoading } from '../store/global'
 
 const API_ENV = {
   production: '',
@@ -14,10 +15,14 @@ const httpClient = axios.create({
   baseURL: API_ENV.local
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.response.use((response) => {
+  setLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = error.response.status = 0 || error.response.status === 500
 
   if (canThrowAnError) {
+    setLoading(false)
     throw new Error(error.message)
   }
 
@@ -26,6 +31,7 @@ httpClient.interceptors.response.use((response) => response, (error) => {
   if (error.status === 401) {
     router.push({ name: 'Home' })
   }
+  setLoading(false)
   return error
 })
 
@@ -38,6 +44,7 @@ export default {
 // Toda request vai enviar essae valor
 
 httpClient.interceptors.request.use((config) => {
+  setLoading(true)
   // Pegando token no LocalStorage
   const token = window.localStorage.getItem('token')
 
