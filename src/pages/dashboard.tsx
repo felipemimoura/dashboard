cb23887e1bf27972d2463453c1269d4b36209fff
Felipe Moura <felipe.moura@pactoenergia.com.br>
2023-04-14 11:02:19 -0300

    feat: create a dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Dashboard () {
+    return (
+        <Flex direction="column" height="100vh">
+            <h1>dash</h1>
+        </Flex>
+    )
+}
