e4cf646664d16a7f5d533990065ec817e10be0b3
Felipe Moura <felipe.moura@pactoenergia.com.br>
2023-04-14 14:40:52 -0300

    feat: create component activelink

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,35 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { ReactElement, cloneElement } from "react";
+
+type ActiveLinkProps = LinkProps & {
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
+};
+
+export const ActiveLink = ({
+  children,
+  shouldMatchExactHref,
+  ...rest
+}: ActiveLinkProps) => {
+  const { asPath } = useRouter();
+
+  let isActive = false
+  if(shouldMatchExactHref && (asPath === rest.href || asPath == rest.as)){
+    isActive = true
+  }
+
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(rest.href.toString()) ||
+      !!rest.as && asPath.startsWith(rest.as.toString()))
+  ) {
+    isActive = true;
+  }
+
+  return(
+    <Link {...rest}>
+    {cloneElement(children,{color: isActive ? 'green' : 'red'})}
+    </Link>
+  )
+};
