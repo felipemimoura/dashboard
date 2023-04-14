04189d215b802fb2b43698fae9336499ad8145e7
Felipe Moura <felipe.moura@pactoenergia.com.br>
2023-04-14 16:45:06 -0300

    refactor: use Sidebar in index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,13 @@
-import { Stack } from "@chakra-ui/react"
-import { NavSection } from "./NavSection"
-import { NavLink } from "./NavLink"
-import {RiDashboardLine} from "react-icons/ri"
+import { Stack } from "@chakra-ui/react";
+import { NavSection } from "./NavSection";
+import { NavLink } from "./NavLink";
+import { RiDashboardLine } from "react-icons/ri";
+import { SidebarNav } from "./SidebarNav";
 
 export const Sidebar = () => {
-    return(
-        <Stack spacing="12" alignItems="center" border="2px">
-            <NavSection title="Cadastro de aplicações">
-            <NavLink href="/dashboard" icon={RiDashboardLine}>
-                Dashboard
-            </NavLink>
-            </NavSection>
-        </Stack>
-    )
-}
+  return (
+    <Stack spacing="12" alignItems="center">
+      <SidebarNav />
+    </Stack>
+  );
+};
