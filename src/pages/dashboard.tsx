e4ef7d4a83478cca85d10d316a1d5b5366d7d723
Felipe Moura <felipe.moura@pactoenergia.com.br>
2023-04-14 11:57:34 -0300

    feat: add header to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/Header";
 import { Flex } from "@chakra-ui/react";
 
-export default function Dashboard () {
-    return (
-        <Flex direction="column" height="100vh">
-            <h1>dash</h1>
-        </Flex>
-    )
+export default function Dashboard() {
+  return (
+    <Flex direction="column" height="100vh">
+      <Header />
+    </Flex>
+  );
 }
