00c1c61721fc4a7c56dbfe86cc482af80102bc34
Felipe Moura <felipe.moura@pactoenergia.com.br>
2023-04-14 14:00:55 -0300

    refactor: remove Icon Button when is mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
-import { Flex, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  IconButton,
+  Image,
+  useBreakpointValue,
+  Icon,
+} from "@chakra-ui/react";
 import { Profile } from "../Profile";
+import { RiMenuLine } from "react-icons/ri";
 
 export const Header = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       as="header"
@@ -11,15 +23,26 @@ export const Header = () => {
       marginX="auto"
       marginTop="4"
       paddingX="6"
-      align={"center"}
-      border={"2px"}
+      align="center"
+      border="2px"
     >
-      <Image
-        src="logo.svg"
-        alt="Pacto Energia"
-        boxSize="150px"
-        objectFit="fill"
-      />
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          marginRight="2"
+          variant="unstyled"
+        ></IconButton>
+      )}
+      {isWideVersion && (
+        <Image
+          src="logo.svg"
+          alt="Pacto Energia"
+          boxSize="150px"
+          objectFit="fill"
+        />
+      )}
       <Flex align="center" marginLeft="auto">
         <Profile />
       </Flex>
